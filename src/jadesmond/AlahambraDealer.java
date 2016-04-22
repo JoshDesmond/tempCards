cb4e6214759c88d8563381f31ed1730c092cdfd4
SkiingxMoose <jodesmond@live.com>
2016-04-22 14:53:00 -0400

    tiny change

--- src/jadesmond/AlahambraDealer.java
+++ src/jadesmond/AlahambraDealer.java
@@ -3,6 +3,6 @@ package jadesmond;
 public interface AlahambraDealer {
 
 	/** Deals all of the cards from the deck out to the game */
-	public abstract void dealCards(Alahambra alahambra);
+	public void dealCards(Alahambra alahambra);
 
 }
